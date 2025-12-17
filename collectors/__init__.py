d30dc45bc98ac2267c4e4730f7eba124b5c17e2e
xingchui <2209802631@qq.com>
2025-12-17 15:29:37 +0800

    Fix: Add intel_cpu_collector into collectors list

--- collectors/__init__.py
+++ collectors/__init__.py
@@ -2,10 +2,10 @@ from typing import Type, Dict
 
 from .gpu.nvidia_gpu_collector import NvidiaGPUCollector
 from .base_collector import BaseCollector
-# from .cpu.intel_cpu_collector import IntelCPUCollector
+from .cpu.intel_cpu_collector import IntelCPUCollector
 
 # Import all collectors here
 COLLECTORS: Dict[str, Type[BaseCollector]] = {
-    # "intel_cpu_collector": IntelCPUCollector,
+    "intel_cpu_collector": IntelCPUCollector,
     "nvidia_gpu_collector": NvidiaGPUCollector,
 }
